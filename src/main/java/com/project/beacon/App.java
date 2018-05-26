5c14994d2b2e53319536a158b9e4a8714fbcf686
huazhoucheng <jinffeng@126.com>
2018-05-26 21:19:43 +0800

    提交

---
+++ src/main/java/com/project/beacon/App.java
@@ -0,0 +1,20 @@
+package com.project.beacon;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+       logger.info("打印日誌 info ...");
+
+    }
+}
